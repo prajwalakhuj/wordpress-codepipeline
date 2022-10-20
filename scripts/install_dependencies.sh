9bf58633d1859d80fe2853d81bb84c9ef51e79bd
Ubuntu <ubuntu@ip-10-0-0-177.us-east-2.compute.internal>
2022-10-20 04:52:36 +0000

    Updated scripts

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+apt update -y
+apt install php7.4 php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+apt install -y nginx
---
+++ scripts/stop_server.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+isExistApp=pgrep nginx
+if [[ -n $isExistApp ]]; then
+systemctl stop nginx.service
+fi
